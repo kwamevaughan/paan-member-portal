09686ef831439ce680b694fdd06a8c9ff12b3096
Emmanuel Eshun <105230244+kwamevaughan@users.noreply.github.com>
2025-06-15 06:15:42 +0000

    rm: unused

--- src/pages/admin/blogs.js
+++ src/pages/admin/blogs.js
@@ -1,19 +1,19 @@
-import React, { useCallback } from 'react';
+// import React, { useCallback } from 'react';
 
-const handleCreateBlog = useCallback(() => {
-  setSelectedIds([]);
-  setShowForm(true);
-}, []);
+// const handleCreateBlog = useCallback(() => {
+//   setSelectedIds([]);
+//   setShowForm(true);
+// }, []);
 
-const handleEditClick = useCallback(async (blog) => {
-  console.log('handleEditClick called with blog:', blog);
-  setSelectedIds([]);
-  setSelectedItemId(blog.id);
-  setShowForm(true);
-  handleEdit(blog);
-}, [handleEdit]);
+// const handleEditClick = useCallback(async (blog) => {
+//   console.log('handleEditClick called with blog:', blog);
+//   setSelectedIds([]);
+//   setSelectedItemId(blog.id);
+//   setShowForm(true);
+//   handleEdit(blog);
+// }, [handleEdit]);
 
-const handleCancel = useCallback(() => {
-  setShowForm(false);
-  setSelectedIds([]);
-}, []); 
+// const handleCancel = useCallback(() => {
+//   setShowForm(false);
+//   setSelectedIds([]);
+// }, []); 

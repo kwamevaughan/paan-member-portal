7943fa62e7d688713af0b53e77fe57ffa06ba50f
Emmanuel Eshun <105230244+kwamevaughan@users.noreply.github.com>
2025-08-19 12:30:20 +0000

    updated link to calendly

--- src/pages/legal-compliance.js
+++ src/pages/legal-compliance.js
@@ -308,7 +308,7 @@ export default function LegalCompliance({ mode = "light", toggleMode }) {
                 Need legal help in Africa?
               </span>
               <button 
-                onClick={handleContactClick}
+                onClick={() => window.open("https://calendly.com/matagaro-sironga-wecomplylabs/30min?month=2025-08", "_blank")}
                 className="px-6 py-2 bg-paan-red hover:bg-paan-red/80 text-white rounded-full w-full md:w-auto transition-all duration-300"
               >
                 Book a Free Strategy Call
